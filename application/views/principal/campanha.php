b368bb33763b773054ce9baa56b910508429e9e9
cachorro <cachorro>
2015-12-12 16:41:44 -0200

    criei a pagina irmao

---
+++ application/views/principal/campanha.php
@@ -0,0 +1,12 @@
+<div class="jumbotron">
+    <h1>Criando uma campanha</h1>
+    <h2>Para se lançar como candidato, primeiramente é preciso se logar através de seu facebook.
+    É preciso ter em mente,porém, que o sucesso ou insucesso em sua campanha muito depende do grau de comprometimento
+    com os eleitores. Assim, mais do que uma campanha recheada de propostas, é preciso resultados e compromisso. Nossa plataforma procura dar espaço àqueles candidatos
+    ainda não tão expressivos no jogo político, mas que procuram uma <i>oportunidade</i> e <i>espaço</i> para crescerem politicamente através de
+    seus <i>atos,realizações</i>, baseados sempre na clareza e compromisso com os seus eleitores.</h2><br>
+    <ul class="pager">
+        <a href="#" class="btn btn-primary btn-lg">Lance sua campanha!</a>
+
+</ul>
+</div>
