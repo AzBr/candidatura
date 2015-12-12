3cb26e288e18a6ce64716e4f8cfbb2c95499fee1
cachorro <cachorro>
2015-12-12 16:36:31 -0200

    criei a pagina irmao

--- application/views/politicos/listagem.php
+++ application/views/politicos/listagem.php
@@ -12,6 +12,7 @@
         <div class="panel panel-primary">
             <div class="panel-heading">
                 <h3 class="panel-title">Estados</h3>
+
             </div>
             <div class="panel-body">
                 Panel content
